d1bfdc1ca732fd7275a71d7beff792db72d880c5
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-05-12 20:22:34 -0300

    Correction to Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ({ onRouteChange, isSignedIn, route }) => {
     return (
         <nav style={{display: "flex", justifyContent: "flex-end"}}>
             {isSignedIn
-                ? <p onClick={() => onRouteChange('SignOut')} className="f3 link dim black underline pa3 pointer">Sign out</p>
+                ? <p onClick={() => onRouteChange('SignIn')} className="f3 link dim black underline pa3 pointer">Sign out</p>
                 : ( route === 'SignIn')
                     ? <p onClick={() => onRouteChange('Register')} className="f4 link dim black underline pa3 pointer">Register</p>
                     : <p onClick={() => onRouteChange('SignIn')} className="f3 link dim black underline pa3 pointer">Sign In</p>
