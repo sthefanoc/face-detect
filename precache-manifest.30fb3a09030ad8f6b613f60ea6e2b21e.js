6b29d0dc9641ac078572a91886a37b1eb11c2c7a
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-05-20 21:07:05 -0300

    Updates

---
+++ precache-manifest.30fb3a09030ad8f6b613f60ea6e2b21e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589723957be5a57137b4e253d9d819e3",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "7f21eed23d5db7e5cd96",
+    "url": "/face-detect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "faa04a7484d8b29910b6",
+    "url": "/face-detect/static/css/main.e5a32347.chunk.css"
+  },
+  {
+    "revision": "7f21eed23d5db7e5cd96",
+    "url": "/face-detect/static/js/2.e7c9be8b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detect/static/js/2.e7c9be8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa04a7484d8b29910b6",
+    "url": "/face-detect/static/js/main.9a8ef0f5.chunk.js"
+  },
+  {
+    "revision": "7772c6b2eff1e4e05260",
+    "url": "/face-detect/static/js/runtime-main.875d89ed.js"
+  }
+]);
--- src/test-app.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class TestApp extends Component {
-    render() {
-        return React.createElement('h1',null, 'helloc component');
-    }
-}
-
-export default TestApp;
